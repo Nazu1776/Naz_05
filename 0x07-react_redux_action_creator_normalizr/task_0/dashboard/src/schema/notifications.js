848b194f4e22a13e9b79055c47072c7432fc2fd9
Nazrawi Asrat <nasrat2676@gmail.com>
2024-08-20 16:57:53 +0000

    Read data from a JSON

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,9 @@
+// schema/notifications.js
+import * as notificationsData from '../notifications.json';
+
+export function getAllNotificationsByUser(userId) {
+  return notificationsData.default.filter(
+    (notification) => notification.author.id === userId
+  ).map((notification) => notification.context);
+}
+
