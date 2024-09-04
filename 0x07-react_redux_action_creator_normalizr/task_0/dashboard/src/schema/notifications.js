d56c213d4bd418ec9782a584afbd4da52e7ff649
Nazrawi Asrat <nasrat2676@gmail.com>
2024-09-04 09:07:39 +0000

    Move directory from source-repo to destination-repo

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,9 +0,0 @@
-// schema/notifications.js
-import * as notificationsData from '../notifications.json';
-
-export function getAllNotificationsByUser(userId) {
-  return notificationsData.default.filter(
-    (notification) => notification.author.id === userId
-  ).map((notification) => notification.context);
-}
-
